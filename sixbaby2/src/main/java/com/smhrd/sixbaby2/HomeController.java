ddb2787cf8ecaa1d1d5b8268a830bf4cd3cb7e45
smhrd <smhrd@DESKTOP-I15MAGD>
2021-08-03 14:35:00 +0900

    1차적으로 복구

---
+++ sixbaby2/src/main/java/com/smhrd/sixbaby2/HomeController.java
@@ -0,0 +1,142 @@
+package com.smhrd.sixbaby2;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.smhrd.mapper.BoardsVO;
+import com.smhrd.mapper.Mapper;
+import com.smhrd.mapper.MemberVO;
+
+@Controller
+public class HomeController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		String formattedDate = dateFormat.format(date);
+		model.addAttribute("serverTime", formattedDate);
+		return "home";
+	}
+
+	@RequestMapping(value = "/main.do")
+	public String home() {
+		return "main";
+	}
+
+	@Autowired
+	private Mapper mapper;
+
+	// 1. 회원가입,수정,탈퇴
+	@RequestMapping("/memberInsert.do")
+	public String memberInsert(MemberVO vo) {
+		mapper.memberInsert(vo);
+		return "redirect:/위치.do";
+	}
+
+	@RequestMapping("/memberUpdate.do")
+	public String memberUpdate(MemberVO vo) {
+		mapper.memberUpdate(vo);
+		return "redirect:/위치.do";
+	}
+
+	@RequestMapping("/memberDelete.do")
+	public String memberDelete(MemberVO vo) {
+		mapper.memberDelete(vo);
+		return "redirect:/위치.do";
+	}
+
+	// 2. 로그인
+	@RequestMapping("/login.do")
+	public String login(MemberVO vo) {
+		mapper.login(vo);
+		return "redirect:/위치.do";
+	}
+
+/**	// 3. 커뮤니티(전체, 열람, 작성, 수정, 삭제)
+	@RequestMapping("/boardList.do")
+	public String boardList(HttpServletRequest request) {
+		List<BoardsVO> list = mapper.boardList();
+		request.setAttribute("list", list);
+		return "redirect:/위치.do";
+	}
+
+	@RequestMapping("/boardContent.do")
+	public String boardContent(@RequestParam("board_no") int board_no, Model model) {
+		BoardsVO vo = mapper.boardContent(board_no);
+		model.addAttribute("vo", vo);
+		return "redirect:/위치.do";
+	}
+
+	@RequestMapping("/boardInsert.do")
+	public String boardInsert(BoardsVO vo) {
+		mapper.boardInsert(vo);
+		return "redirect:/위치.do";
+	}
+
+	@RequestMapping("/boardUpdate.do")
+	public String boardUpdate(BoardsVO vo) {
+		mapper.boardUpdate(vo);
+		return "redirect:/위치.do";
+	}
+
+	@RequestMapping("/boardDelete.do")
+	public String boardDelete(@RequestParam("board_no") int board_no) {
+		mapper.boardDelete(board_no);
+		return "redirect:/위치.do";
+	}
+**/	
+
+	/*
+	 * @RequestMapping("/boardForm.do") public String boardForm() { return
+	 * "boardForm"; // boardForm.jsp }
+	 */
+
+
+
+	// 템플릿적용
+	@RequestMapping(value = "/events.do")
+	public String events() {
+		return "events";
+	}
+
+	@RequestMapping(value = "/families.do")
+	public String families() {
+		return "families";
+	}
+
+	@RequestMapping(value = "/community.do")
+	public String seremons() {
+		return "community";
+	}
+	
+	@RequestMapping(value = "/join.do")
+	public String join() {
+		return "join";
+	}
+	
+	
+
+
+	// iot 테스트용 페이지
+	@RequestMapping("/iot.do")
+	public String iot() {
+		return "iot";
+	}
+}
---
+++ sixbaby2/target/classes/com/smhrd/mapper/project.sql
@@ -0,0 +1,44 @@
+create table member
+(id varchar(12) not null, pw varchar(20) not null,
+nickname varchar(20) not null, name varchar(50) not null,
+gender varchar(50) not null, birth varchar(50) not null,
+tell varchar(50) not null,
+primary key(id));
+
+create table baby
+(baby_num int not null auto_increment, id varchar(12) not null,
+baby_name varchar(50) not null, baby_birth varchar(50) not null,
+baby_gender varchar(50) not null, 
+primary key(baby_num));
+
+create table sound
+(id varchar(12), parents_snd varchar(20));
+
+create table boards
+(board_no int not null auto_increment, category varchar(50) not null,
+title varchar(50) not null, nickname varchar(20) not null,
+picture varchar(50), contents varchar(500) not null,
+indate datetime default now(),
+primary key(board_no));
+
+create table comments
+(comment_no int not null auto_increment, category varchar(50) not null,
+nickname varchar(20) not null, contents varchar(500) not null,
+likes int default 0,
+primary key(comment_no));
+
+create table babycondition
+(condition_num int not null auto_increment, baby_num int not null,
+hungry int, pup int, burping int, cold_hot int,
+condition_date datetime default now(), 
+primary key(condition_num));
+
+insert into member
+values('test', '1234', 'user', '스마트', '남자', '00-01-01', '010-1111-1111');
+
+insert into boards(category, title, nickname, picture, contents)
+values('꿀팁', 'test', '스마트', 'cat.jpg', '가나다라마바사아자차카타파하');
+
+select * from boards;
+
+select * from member;
